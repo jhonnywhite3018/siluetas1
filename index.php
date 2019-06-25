62bf4860f3b105abbb3a168416a482b8de0c5dfe
Jhonnywhite2000 <jhonnywhite3018@gmail.com>
2019-06-25 16:14:35 +0200

    Version inicial

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+if(isset($_GET['silueta']))
+	$_GET['silueta']++;
+else $_GET['silueta']=1;
+
+if($_GET['silueta']>7)
+	$_GET['silueta']=1;
+
+$txt = '
+	<div>
+	<a href="?silueta='.$_GET['silueta'].'">
+		<img src="figs/f'.$_GET['silueta'].'.jpg" style="height:150px">
+	</a>
+	</div>
+	
+';
+echo $txt;
+echo $txt;
+echo $txt;
+echo $txt;
+
+?>
